c0757aa7a4a3feea9e815775157c42f78dce4568
said9981 <34682133+said9981@users.noreply.github.com>
2017-12-19 16:08:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {     
+
+      
+
+        static void Main(string[] args)
+        {
+
+            string isim;
+            Console.Write("isminizi giriniz lütfen:");
+            isim = Console.ReadLine();
+            foreach (char ad in isim)
+            {
+                Console.Write((char)(ad + 10));
+            }
+            Console.ReadLine();
+        }
+    }
+}
